febd936786e83db4f6af025d3a4c17a564366c8e
Dennis Freyer <dennis.dfry@gmail.com>
2025-01-03 21:56:51 +0100

    mainpage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MainpageComponent } from './mainpage/mainpage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MainpageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"dennis-freyer","appId":"1:346204051209:web:d001c7af30b90ab96025d8","storageBucket":"dennis-freyer.firebasestorage.app","apiKey":"AIzaSyApoJqRrPYiIz5Qp3gLbhqhdiUA2dGjfzM","authDomain":"dennis-freyer.firebaseapp.com","messagingSenderId":"346204051209"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
