2da3df449ac663918e4167b4823cbcd8d10caa14
Dennis Freyer <dennis.dfry@gmail.com>
2025-01-07 15:20:06 +0100

    update

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { AboutMeComponent } from '../about-me/about-me.component';
+import { TechnologiesComponent } from '../technologies/technologies.component';
 
 @Component({
   selector: 'app-mainpage',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, AboutMeComponent, TechnologiesComponent ],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.scss'
 })
