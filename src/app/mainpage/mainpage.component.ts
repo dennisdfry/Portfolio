1aa062e81b7cb6d26e36a43123086c896783d9be
Dennis Freyer <dennis.dfry@gmail.com>
2025-01-07 21:04:44 +0100

    technologies and portfolio

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { AboutMeComponent } from '../about-me/about-me.component';
 import { TechnologiesComponent } from '../technologies/technologies.component';
+import { PortfolioComponent } from '../portfolio/portfolio.component';
 
 @Component({
   selector: 'app-mainpage',
   standalone: true,
-  imports: [HeaderComponent, AboutMeComponent, TechnologiesComponent ],
+  imports: [HeaderComponent, AboutMeComponent, TechnologiesComponent, PortfolioComponent ],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.scss'
 })
