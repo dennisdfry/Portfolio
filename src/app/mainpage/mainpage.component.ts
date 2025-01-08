ae3251b1da9de0ad0587970f884357f4917eeb4c
Dennis Freyer <dennis.dfry@gmail.com>
2025-01-08 16:31:32 +0100

    contact me

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from '../header/header.component';
 import { AboutMeComponent } from '../about-me/about-me.component';
 import { TechnologiesComponent } from '../technologies/technologies.component';
 import { PortfolioComponent } from '../portfolio/portfolio.component';
-
+import { FeedbackComponent } from '../feedback/feedback.component';
+import { ContactMeComponent } from '../contact-me/contact-me.component';
 @Component({
   selector: 'app-mainpage',
   standalone: true,
-  imports: [HeaderComponent, AboutMeComponent, TechnologiesComponent, PortfolioComponent ],
+  imports: [HeaderComponent, AboutMeComponent, TechnologiesComponent, PortfolioComponent, FeedbackComponent, ContactMeComponent ],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.scss'
 })
