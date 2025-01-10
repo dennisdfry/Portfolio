c6301f49da182d91ca4858c97a13940b7b955542
Dennis Freyer <dennis.dfry@gmail.com>
2025-01-10 17:45:09 +0100

    feedback component  update

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-feedback',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './feedback.component.html',
   styleUrl: './feedback.component.scss'
 })
 export class FeedbackComponent {
+  comments = ['hallo', 'huhu', 'tschüss'];
 
+  currentIndex = 1;
+
+  prevComment(): void {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+  }
+
+  nextComment(): void {
+    if (this.currentIndex < this.comments.length - 1) {
+      this.currentIndex++;
+    }
+  }
+
+  getCommentClass(index: number): string {
+    return index === this.currentIndex ? 'comment_center' : '';
+  }
 }
