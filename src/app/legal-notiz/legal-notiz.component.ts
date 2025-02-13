4d4748ae66e30df5c40795b3ce178c4ee0f59118
Dennis Freyer <dennis.dfry@gmail.com>
2025-02-13 21:11:37 +0100

    legal notiz js docs

--- src/app/legal-notiz/legal-notiz.component.ts
+++ src/app/legal-notiz/legal-notiz.component.ts
@@ -11,17 +11,31 @@ import { CommonModule } from '@angular/common';
 })
 export class LegalNotizComponent implements OnInit {
 
+  /**
+   * Flag indicating if the legal notice is visible.
+   */
   isVisible = false;
 
+  /**
+   * Creates an instance of LegalNotizComponent.
+   * @param legalNotizService The service for controlling the visibility of the legal notice.
+   */
   constructor(private legalNotizService: LegalNotizService) {}
 
+  /**
+   * Initializes the component and subscribes to the visibility state of the legal notice.
+   * Sets `isVisible` based on the value emitted by `LegalNotizService`.
+   */
   ngOnInit(): void {
     this.legalNotizService.visibility$.subscribe(visible => {
       this.isVisible = visible;
     });
   }
 
+  /**
+   * Closes the legal notice by calling the `hide()` method of `LegalNotizService`.
+   */
   closeDatenschutz(): void {
-    this.legalNotizService.hide(); 
+    this.legalNotizService.hide();
   }
 }
