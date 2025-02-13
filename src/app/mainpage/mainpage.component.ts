75931768de4017d9e93bdb24cbd9608c163247c0
Dennis Freyer <dennis.dfry@gmail.com>
2025-02-13 21:13:05 +0100

    mainpage jsdocs

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -20,26 +20,47 @@ import { LegalNotizService } from '../legal-notiz.service';
   styleUrl: './mainpage.component.scss'
 })
 export class MainpageComponent  {
+
+  /**
+   * Creates an instance of MainpageComponent.
+   * @param translateConfigService The service for handling language translation.
+   * @param legalNotizService The service for controlling the visibility of the legal notice.
+   */
   constructor(private translateConfigService: TranslateConfigService,
     public legalNotizService: LegalNotizService 
   ) {}
+
+  /**
+   * Scrolls the page to a section specified by its ID.
+   * @param sectionId The ID of the section to scroll to.
+   */
   scrollToSection(sectionId: string): void {
-    console.log(sectionId)
+    console.log(sectionId);
     const element = document.getElementById(sectionId);
-    console.log(element)
+    console.log(element);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
   }
 
+  /**
+   * Handles language change by calling `changeLanguage` method from `TranslateConfigService`.
+   * @param lang The new language code (e.g., 'en' or 'de').
+   */
   onLanguageChanged(lang: string) {
     this.translateConfigService.changeLanguage(lang);
   }
 
+  /**
+   * Opens the legal notice by calling the `show` method from `LegalNotizService`.
+   */
   openLegalNotiz() {
     this.legalNotizService.show();
   }
 
+  /**
+   * Closes the legal notice by calling the `hide` method from `LegalNotizService`.
+   */
   closeLegalNotiz() {
     this.legalNotizService.hide();
   }
