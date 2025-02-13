1372a3d1cd6d6b15d31dede8d7ec887e7f89ae83
Dennis Freyer <dennis.dfry@gmail.com>
2025-02-13 21:10:25 +0100

    header js docs

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -13,19 +13,48 @@ import { TranslateModule } from '@ngx-translate/core';
 })
 export class HeaderComponent {
 
+  /**
+   * Event emitted when the language is changed.
+   */
   @Output() languageChanged = new EventEmitter<string>();
 
-
+  /**
+   * Flag indicating if the dropdown menu is open.
+   */
   isDropdownOpen = false;
+
+  /**
+   * Flag indicating if the default language is currently selected.
+   */
   isDefaultLanguage = false;
+
+  /**
+   * Default image path for the language toggle button.
+   */
   defaultImage = "../../assets/img/header/Property 1=Default@2x.png";
 
+  /**
+   * Creates an instance of HeaderComponent.
+   * @param translateConfigService The service for handling language changes.
+   */
   constructor(private translateConfigService: TranslateConfigService) { }
+
+  /**
+   * Event emitted when scrolling to a section.
+   */
   @Output() scrollToSection = new EventEmitter<string>();
-  
+
+  /**
+   * Toggles the state of the dropdown menu.
+   */
   openDropDown() {
     this.isDropdownOpen = !this.isDropdownOpen;
   }
+
+  /**
+   * Closes the dropdown menu when a click occurs outside of it.
+   * @param event The click event.
+   */
   @HostListener('document:click', ['$event'])
   closeDropdownOnClickOutside(event: Event) {
     const dropdown = document.querySelector('.dropdown-menu');
@@ -38,10 +67,18 @@ export class HeaderComponent {
     }
   }
 
+  /**
+   * Emits an event to scroll to a specific section of the page.
+   * @param sectionId The ID of the section to scroll to.
+   */
   onScroll(sectionId: string) {
     this.scrollToSection.emit(sectionId);
   }
 
+  /**
+   * Switches the website's language between English and German.
+   * @param lang The language to switch to ('en' for English, 'de' for German).
+   */
   switchLanguage(lang: string) {
     if (this.isDefaultLanguage) {
       lang = 'en';
