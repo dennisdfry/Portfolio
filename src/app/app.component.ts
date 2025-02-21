45554a6168188e1af63175eb1a2e8ff19bc34a85
Dennis Freyer <dennis.dfry@gmail.com>
2025-02-21 20:31:08 +0100

    favicon in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { MainpageComponent } from './mainpage/mainpage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainpageComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'portfolio-website';
+  ngOnInit() {
+    this.setFavicon('assets/img/favicon.png');
+  }
+  
+  setFavicon(url: string) {
+    // Entferne das vorhandene Favicon
+    const existingFavicon = document.querySelector('link[rel="icon"]');
+    if (existingFavicon) {
+      existingFavicon.remove();
+    }
+
+    // Erstelle ein neues Favicon
+    const favicon = document.createElement('link');
+    favicon.rel = 'icon';
+    favicon.type = 'image/png';
+    favicon.href = url;
+
+    // Füge das neue Favicon dem <head> hinzu
+    document.head.appendChild(favicon);
+  }
 }
